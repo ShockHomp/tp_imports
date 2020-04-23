b663cf823e79eda59780eb64cab425e4894c157a
liyufeng <920929621@qq.com>
2020-04-24 05:55:13 +0800

    修改

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -95,9 +95,7 @@ class Index
 ###############    （监听失败？
 //5.0支持存储过程，如果我们定义了一个数据库存储过程sp_query，可以使用下面的方式调用：
 //        $result = Db::query('call sp_query(8)');
-<<<<<<< HEAD
-		return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span styl="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
-=======
+
         //闭包查询方法（推荐使用闭包查询包括两种方法）
         $openid = 'ojvXN4q458ffOfDEMph-xJS3cc58';
         $id = '2407';
@@ -112,8 +110,7 @@ class Index
         $columnDb = Db::table('shopping_buyer_users')->where('openid', $openid)->column('id,openid,shop_id');
         dump($columnDb);
 //        dump($bbDb);
-//		return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span styl="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
->>>>>>> ff0cc3029398161c0328e4d5b1dbdb9df1f5fb1a
+
     }
 
     public
@@ -501,7 +498,7 @@ class Index
 //        return $buyerUsers->openid;//调用模型中的修改器
         $buyerUsers = new BuyerUsers();
         $buyerUsers->openid = 'ss';
-        $buyerUsers->save();//(只有save()方法可以调用修改器)除了赋值的方式可以触发修改器外，还可以用下面的方法批量触发修改器：
+//        $buyerUsers->save();//(只有save()方法可以调用修改器)除了赋值的方式可以触发修改器外，还可以用下面的方法批量触发修改器：
         return '修改openid为'.$buyerUsers->openid;
 
 
