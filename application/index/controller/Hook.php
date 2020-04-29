b3423698015684bb526473197da67f86438d8029
liyufeng <920929621@qq.com>
2020-04-29 21:16:06 +0800

    request

--- application/index/controller/Hook.php
+++ application/index/controller/Hook.php
@@ -3,14 +3,30 @@ namespace app\index\controller;
 
 use think\View;
 use think\Controller;
-
+use think\Model\User;
 // use app\index\model\New;
 use think\Request;
 use think\db\Query;
 
 class Hook extends controller
 {
-	public function test(){
+
+
+    /**
+     * Hook constructor.
+     * @param $request
+     */
+    protected $request;
+    protected $user;//依赖注入 可以直接使用model 类里面的 方法（可以操作数据库表字段）
+
+    public function __construct(Request $request, User $user)
+    {
+        $this->request = $request;
+        $this->user = $user;
+    }
+
+
+    public function test(){
 		return "this is test";
 	}
 	public function demo1(){
@@ -26,6 +42,11 @@ class Hook extends controller
 	}
 	public function bd(){
 	    return 'bind自动绑定';
+    }
+    //依赖注入
+    public function req(Request $request)
+    {
+        return  '学习课程:'.$request->param('lesson');
     }
 
 }
