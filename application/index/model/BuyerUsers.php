6a2e4bf4dd2373a7a7aa803d4dc3f7c409bce558
920929621@qq.com <Q936766q>
2020-04-30 08:11:35 +0800

    读取器

--- application/index/model/BuyerUsers.php
+++ application/index/model/BuyerUsers.php
@@ -3,6 +3,14 @@
 namespace app\index\model;
 use think\Model;
 class BuyerUsers extends Model{
+
+
+    // 关闭自动写入update_time字段
+    protected $updateTime = false;
+    // 设置当前模型对应的完整数据表名称
+//    protected $table = '';
+
+
     public function getUser(){
         return $this->getData('openid');
     }
